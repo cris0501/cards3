f62183905805e96c483f7336da69bc446c88e559
Server <cristian050198@outlook.com>
2024-02-09 08:07:47 +0000

    Fix: return success code

--- sw.sh
+++ sw.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-files=""
+files="'/'"
 
 cd 'dist'
 for file in '.'/*; do
   file=$(basename "$file")
   files="$files,\n'$file'"
 done
-files=$(echo -e "$files" | sed '1d')
-echo $files
+#files=$(echo -e "$files" | sed '1d')
 
 cd ..
 awk -v files="$files" '{gsub(/\$\{shell\}/, files)} 1' src/sw.js > dist/sw.js
+exit 0

a9686bd42c1dd8ab3dde232df8884e2b84a3a32c
Server <cristian050198@outlook.com>
2024-02-09 08:04:51 +0000

    Add: Create sw

---
+++ sw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+files=""
+
+cd 'dist'
+for file in '.'/*; do
+  file=$(basename "$file")
+  files="$files,\n'$file'"
+done
+files=$(echo -e "$files" | sed '1d')
+echo $files
+
+cd ..
+awk -v files="$files" '{gsub(/\$\{shell\}/, files)} 1' src/sw.js > dist/sw.js
